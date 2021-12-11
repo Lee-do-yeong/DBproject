302816b899375a7478d611a13b213096aea411cc
Lee-do-yeong <dlehdud98@nate.com>
2021-12-11 14:43:46 +0900

    현재위치 구현 완료, road 부분 오류 수정

--- app/src/main/java/org/techtown/mycalendar/MainActivity.java
+++ app/src/main/java/org/techtown/mycalendar/MainActivity.java
@@ -1,14 +1,24 @@
 package org.techtown.mycalendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PointF;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,10 +37,12 @@ import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Intent intent;
     String value;
     FloatingActionButton fab;
@@ -38,6 +50,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TMapPoint tMapPointStart;
     TMapPoint tMapPointEnd = null;
     private GpsTracker gpsTracker;
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,45 +64,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tmapview = new TMapView(this);
         tmapview.setSKTMapApiKey("l7xx9c10fb3dbac642078ebac04bd35fba5c");
 
+        if (!checkLocationServicesStatus()) { showDialogForLocationServiceSetting(); }
+        else {checkRunTimePermission(); }
+
         fab = findViewById(R.id.fab_add);
         fab.setOnClickListener(this);
 
         gpsTracker = new GpsTracker(MainActivity.this);
 
-        List<Data> toiletList=initLoadToiletDatabase();
-        addToiletMarker(toiletList);
-
         initialize(tmapview);
     }
 
-    public void onClick(View v) {
-        Intent intent;
-
-        switch (v.getId()) {
-            //풍선뷰 화면 설정..
-            case R.id.fab_add:
-                //intent = getIntent();
-                //String location = intent.getExtras().getString("location");
-
-                //ArrayList<String> arrBuilding = new ArrayList<>();
-                //arrBuilding.add(location);
-                //searchPOI(arrBuilding);
-                fab.setVisibility(View.INVISIBLE); //마커 설정 후 버튼 사라짐
-                break;
+    public void onClick(View v) { // 플로팅 버튼 클릭 시에 화장실 마커 표시(플로팅 버튼은 사라짐)
+        if(v.getId() == R.id.fab_add){
+            List<Data> toiletList=initLoadToiletDatabase();
+            addToiletMarker(toiletList);
+            //road(toiletList);
+            fab.setVisibility(View.INVISIBLE); //마커 설정 후 버튼 사라짐
         }
     }
 
-    private void road(ArrayList<TMapPoint> arrTPoint) {
+    private void road(List<Data>toiletList) {
         String point;
 
-        double lat1 = gpsTracker.getLatitude();
-        double lon1 = gpsTracker.getLongitude();
+        double lat1 = 35.846964;
+        double lon1 = 127.129436;
 
-        /*tmapview.setCenterPoint(lon1, lat1);
-        tmapview.setLocationPoint(lon1, lat1);
+        //double lat1 = gpsTracker.getLatitude();
+        //double lon1 = gpsTracker.getLongitude();
+
+        //tmapview.setCenterPoint(lon1, lat1);
+        //tmapview.setLocationPoint(lon1, lat1);
         setMultiMarkers2(lat1, lon1); //현재위치 마커표시*/
 
-        point = arrTPoint.get(0).toString();
+        point = toiletList.get(0).toString();
         Log.d("###", "POI: " + point);
         String lat = (String) point.subSequence(4,15);
         String lon = (String) point.subSequence(20,32);
@@ -137,10 +148,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 전북대로 설정
         tmapview.setOnClickListenerCallBack(mOnClickListenerCallback);
         tmapview.setZoomLevel(15);
+        tmapview.setCenterPoint(127.129436, 35.846964);
+        double lat1 = gpsTracker.getLatitude();
+        double lon1 = gpsTracker.getLongitude();
 
-        /*tmapview.setCenterPoint(lon1, lat1);
+
+        tmapview.setCenterPoint(lon1, lat1);
         tmapview.setLocationPoint(lon1, lat1);
-        setMultiMarkers2(lat1, lon1); //현재위치 마커표시*/
+        setMultiMarkers2(lat1, lon1); //현재위치 마커표시
     }
 
     // 주변 명칭 검색
@@ -197,9 +212,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Bitmap bitmapIcon = createMarkerIcon(R.drawable.poi_dot);
 
         TMapPoint tMapPoint = new TMapPoint(lat, lon);
+
         TMapMarkerItem tMapMarkerItem = new TMapMarkerItem();
         tMapMarkerItem.setIcon(bitmapIcon);
-
+        tMapMarkerItem.setPosition(0.5f,1.0f);
         tMapMarkerItem.setTMapPoint(tMapPoint);
 
         tmapview.addMarkerItem("markerItem", tMapMarkerItem);
@@ -241,4 +257,115 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return false; 
         }
     };
+
+    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults);
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            boolean check_result = true;
+
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            if (check_result) { ; }
+            else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+
+                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    void checkRunTimePermission() {
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) { }
+        else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {
+
+                Toast.makeText(MainActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+            else { ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        }
+    }
+
+    public String getCurrentAddress(double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 7);
+        } catch (IOException ioException) {
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+        }
+
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+        }
+        Address address = addresses.get(0);
+        return address.getAddressLine(0).toString() + "\n";
+    }
+
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 진행하시겠습니까?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case GPS_ENABLE_REQUEST_CODE:
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
 }
